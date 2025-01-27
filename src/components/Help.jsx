95ee16d9d33c84edb612b96d7e5fce12cd83ab23
tejalbaser <tejalbaser@gmail.com>
2025-01-27 22:07:40 +0530

    Update Help.jsx

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 
-function Account(){
+function Help(){
     return(
         <div>
-            <h1>Help & Support</h1>
+            <h2>About Us</h2>
+            <p>Welcome to Fluento Shopping! Your go-to online gift shop.</p>
+
+            <h2>Help & Support</h2>
             <p>If you have any questions, please reach out to us.</p>
+
+            <h2>Contact Us</h2>
+            <p>
+                Email: <a href="mailto:support@fluentoshopping.com">support@fluentoshopping.com</a>
+            </p>
+            <p>
+                Phone: <a href="tel:+1234567890">+1234567890</a>
+            </p>
         </div>
     );
 }
 
-export default Account;
+export default Help;
